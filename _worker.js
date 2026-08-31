9332861a78facc8535597389ae9e5b4deae09f7c
dhaka-livestream <146126511+dhaka-livestream@users.noreply.github.com>
2026-09-01 03:41:43 +0600

    Add new redirect for Orbit-sonyten2hd

--- _worker.js
+++ _worker.js
@@ -297,6 +297,10 @@ export default {
         url: 'http://172.19.17.3:8090/hls/SonyTen5HD.m3u8',
         type: 'redirect'
       },
+      'Orbit-sonyten2hd': {
+        url: 'http://172.19.17.3:8090/hls/SonyTen2hd.m3u8',
+        type: 'redirect'
+      },
       'zee_bangla': {
         url: 'https://bldcmprod-cdn.toffeelive.com/cdn/live/slang/zee_bangla_576/zee_bangla_576.m3u8?bitrate=500000&channel=zee_bangla_576&gp_id=',
         type: 'proxy'
