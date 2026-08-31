5f649343c921d3ac908e88c4745c59331e0d5700
dhaka-livestream <146126511+dhaka-livestream@users.noreply.github.com>
2026-09-01 02:57:35 +0600

    Add worker script for channel proxy and tool blocking
    Implement a worker script to block HTTP Canary and similar tools while providing a proxy for various streaming channels.

---
+++ _worker.js
@@ -0,0 +1,352 @@
+// _worker.js – শুধু HTTP Canary-সদৃশ টুল ব্লক, বাকি সব প্লেয়ার/ব্রাউজার চলবে
+
+export default {
+  async fetch(request) {
+    const url = new URL(request.url);
+    const path = url.pathname;
+    const userAgent = request.headers.get('User-Agent') || '';
+
+    // 🛡️ শুধুমাত্র HTTP Canary ও তার মতো টুল ব্লক (খুব সংকীর্ণ তালিকা)
+    const BLOCKED_AGENTS = [
+      'httpcanary', // HTTP Canary অ্যাপ
+      'okhttp',     // OkHttp লাইব্রেরি (অনেক স্নিফার ব্যবহার করে)
+    ];
+
+    // চেক: ব্লক তালিকায় আছে কিনা (কেস ইনসেনসিটিভ)
+    const isBlocked = BLOCKED_AGENTS.some(agent => userAgent.toLowerCase().includes(agent.toLowerCase()));
+    if (isBlocked) {
+      return new Response('🚫 Access Denied: HTTP Canary or similar tool detected.', { 
+        status: 403,
+        headers: { 'Content-Type': 'text/plain' }
+      });
+    }
+
+    // ============================================================
+    // 📌 চ্যানেল কনফিগারেশন (ডিফল্ট ৬টি)
+    // ============================================================
+    const CHANNELS = {
+      'sunnext-sunnews': {
+        url: 'https://livestream3.sunnxt.com/491c99fb6d0c49e88e6349170d890a2f/SunNewsB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-ktv': {
+        url: 'https://livestream.sunnxt.com/6ae70edd4c1440379f5311e8fbddc7c1/KTVB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-ktvhd': {
+        url: 'https://livestream.sunnxt.com/61477b4c8d8d45d5a49e044cc1dffc60/KTVHDB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-suryatvhd': {
+        url: 'https://livestream15.sunnxt.com/d719fad367614ee5baad747822767ad8/SuryaTVHDB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-suryatv': {
+        url: 'https://livestream6.sunnxt.com/30612a1b269d4a18aa14657641c47515/SuryaTVB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-suryamovies': {
+        url: 'https://livestream.sunnxt.com/e24ee14c395945bd8ccb065e1bce8b9b/SuryaMoviesB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-suryamusic': {
+        url: 'https://livestream.sunnxt.com/8c2352ff54954e7b9a4188045dcf3b27/SuryaMusicB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-suryacomedy': {
+        url: 'https://livestream.sunnxt.com/6505e922bf164423ad122f404747356a/SuryaComedyB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-udayatvhd': {
+        url: 'https://livestream16.sunnxt.com/a8d28f18944c4946ad7133938860e7cf/UdayaTVHDB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-udayatv': {
+        url: 'https://livestream5.sunnxt.com/e2f36b5d0be74780a041a8f5b65bc7e6/UdayaTVB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-udayamovies': {
+        url: 'https://livestream.sunnxt.com/1c02547243c041eea5dab1c343018e90/UdayaMoviesB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-udaymusic': {
+        url: 'https://livestream.sunnxt.com/8034b7519d6a4ab8929aa4279fda1f29/UdayaMusicB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-udayacomedy': {
+        url: 'https://livestream.sunnxt.com/8a3d3d8d679b4f9f83a8305b4ead0644/UdayaComedyB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-geminitv': {
+        url: 'https://livestream4.sunnxt.com/a1a61fa1811c4d20a5c2d5e14cdc0cd2/GeminiTVB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-geminitvhd': {
+        url: 'https://livestream14.sunnxt.com/e778d9c98488494b9c9b38f9c48b63ec/GeminiTVHDB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-geminimovieshd': {
+        url: 'https://livestream.sunnxt.com/ec0d4961a002442295f91efc9d675c9d/GeminiMoviesHDB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-geminimovies': {
+        url: 'https://livestream.sunnxt.com/6a59979ff0044fd3b6e0cb85d6f44432/GeminiMoviesB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-geminimusichd': {
+        url: 'https://livestream.sunnxt.com/6a6520e446604c6e9840e5bf3a3a7d95/GeminiMusicHDB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-geminimusic': {
+        url: 'https://livestream.sunnxt.com/52b94f70c6e64692b2497f5023b629cd/GeminiMusicB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-geminicomedy': {
+        url: 'https://livestream.sunnxt.com/167c40e9521b470b87b4cf921fd0e146/GeminiComedyB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-geminilife': {
+        url: 'https://livestream.sunnxt.com/a4b4f71a8b4344f3a280e906657a517a/GeminiLifeB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-adithyatv': {
+        url: 'https://livestream.sunnxt.com/4d0eb3cde30247ada4ade679fdfbaf86/AdithyaTVB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-kushitv': {
+        url: 'https://livestream.sunnxt.com/4a736d41608849758a0aed36949ded30/KushiTVB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-chuttitv': {
+        url: 'https://livestream.sunnxt.com/3ed29d5b01b546eaa05d184cd87535f1/ChuttiTVB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-kochutv': {
+        url: 'https://livestream.sunnxt.com/1893b9ab790747cb80a584873a608dcb/KochuTVB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-chintutv': {
+        url: 'https://livestream.sunnxt.com/ed4c67ad957644b69361651d9101107e/ChintuTVB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'sunnext-sunbangla': {
+        url: 'https://livestream7.sunnxt.com/bf76ee92dd01473bb2eb57d137294484/SunBanglaB_IN_index.mpd',
+        type: 'redirect'
+      },
+      'zee5-andpictureshd': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Andpictureshd/default/index.mpd',
+        type: 'redirect'
+      },
+      'zee5-zeetvhd': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Zeetvhd/default/index.mpd',
+        type: 'redirect'
+      },
+      'zee5-zeecinemahd': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Zeecinemahd/default/manifest.mpd',
+        type: 'redirect'
+      },
+      'zee5-andpictures': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Andpictures/default/manifest.mpd',
+        type: 'redirect'
+      },
+      'zee5-zeetv': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Zeetv/default/manifest.mpd',
+        type: 'redirect'
+      },
+      'zee5-zeecinema': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/NGCHD/default/NGCHD.m3u8',
+        type: 'redirect'
+      },
+      'zee5-bigmagic': {
+        url: 'https://mumt05.tangotv.in/87NeALx2BIGMAGIC/index.mpd',
+        type: 'redirect'
+      },
+      'zee5-zeeanmol': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Zeeanmol/default/manifest.mpd',
+        type: 'redirect'
+      },
+      'zee5-zeeanmolcinema': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Zeeanmolcinema/default/manifest.mpd',
+        type: 'redirect'
+      },
+      'zee5-zeebollywood': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Zeebollywood/default/manifest.mpd',
+        type: 'redirect'
+      },
+      'zee5-andtvhd': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Andtvhd/default/index.mpd',
+        type: 'redirect'
+      },
+      'zee5-andtv': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Andtv/default/manifest.mpd',
+        type: 'redirect'
+      },
+      'zee5-zing': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Zing/default/manifest.mpd',
+        type: 'redirect'
+      },
+      'zee5-zeeaction': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Zeeaction/default/main.mpd',
+        type: 'redirect'
+      },
+      'zee5-zoom2': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Zoom2/default/manifest.mpd',
+        type: 'redirect'
+      },
+      'zee5-zeebanglacinema': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/ColorsHD/default/ColorsHD.m3u8',
+        type: 'redirect'
+      },
+      'zee5-zeebangla': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Zeebangla/default/manifest.mpd',
+        type: 'redirect'
+      },
+      'zee5-9xm2': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/9xm2/default/main.mpd',
+        type: 'redirect'
+      },
+      'zee5-discoverysd': {
+        url: 'https://d1g8wgjurz8via.cloudfront.net/bpk-tv/Discoverychannel2/default/manifest.mpd',
+        type: 'redirect'
+      },
+      'infotainment-discoverybd': {
+        url: 'http://live.balajibroadband.com:3500/live/3428.m3u8',
+        type: 'redirect'
+      },
+      'infotainment-discoveryhindi': {
+        url: 'http://live.balajibroadband.com:3500/live/3444.m3u8',
+        type: 'redirect'
+      },
+      'infotainment-adventureearth': {
+        url: 'https://autentic-adventure-earth-1-eu.rakuten.wurl.tv/playlist.m3u8',
+        type: 'redirect'
+      },
+      'infotainment-animalplanet': {
+        url: 'https://tiger-hub.vercel.app@vodzong.mjunoon.tv:8087/streamtest/Animal-Planet-158-3/playlist.m3u8',
+        type: 'redirect'
+      },
+      'kids-wowkidz': {
+        url: 'https://live20.bozztv.com/giatvplayout7/giatv-209622/tracks-v1a1/mono.ts.m3u8',
+        type: 'redirect'
+      },
+      'kids-tom&jerry': {
+        url: 'https://live20.bozztv.com/giatvplayout7/giatv-208314/tracks-v1a1/mono.ts.m3u8',
+        type: 'redirect'
+      },
+      'kids-pertyuniversie': {
+        url: 'https://nomawnoijl.gpcdn.net/akash/partyuniverse/playlist.m3u8',
+        type: 'redirect'
+      },
+      'inbangla-akash8': {
+        url: 'https://cdn-4.pishow.tv/live/969/master.m3u8',
+        type: 'redirect'
+      },
+      'inbangla-sunbangla': {
+        url: 'http://live.balajibroadband.com:3500/live/1669.m3u8',
+        type: 'redirect'
+      },
+      'zeebanglabdx3': {
+        url: 'https://stream.ottplus.live/live/zee_bangla_abr/index.m3u8',
+        type: 'redirect'
+      },
+      'starjalshabdx3': {
+        url: 'https://starjalshabdx-anik.pages.dev/proxy/live.m3u8',
+        type: 'redirect'
+      },
+      'zee_bangla': {
+        url: 'https://bldcmprod-cdn.toffeelive.com/cdn/live/slang/zee_bangla_576/zee_bangla_576.m3u8?bitrate=500000&channel=zee_bangla_576&gp_id=',
+        type: 'proxy'
+      },
+      'b4u_music': {
+        url: 'https://bldcmprod-cdn.toffeelive.com/cdn/live/b4u_music/playlist.m3u8',
+        type: 'proxy'
+      },
+      'zee_bangla_cinema': {
+        url: 'https://bldcmprod-cdn.toffeelive.com/cdn/live/zee_bangla_cinema/playlist.m3u8',
+        type: 'proxy'
+      },
+      'andpicture_hd': {
+        url: 'https://bldcmprod-cdn.toffeelive.com/cdn/live/andpicture_hd/playlist.m3u8',
+        type: 'proxy'
+      },
+      'mtv': {
+        url: 'https://bldcmprod-cdn.toffeelive.com/cdn/live/slang/mtv_576/mtv_576.m3u8?bitrate=500000&channel=mtv_576&gp_id=',
+        type: 'proxy'
+      },
+      'sony_aath': {
+        url: 'https%3A%2F%2Fbldcmprod-cdn.toffeelive.com%2Fcdn%2Flive%2Fslang%2Fsonyaath_576%2Fsonyaath_576.m3u8%3Fbitrate%3D1000000%26channel%3Dsonyaath_576%26gp_id%3D',
+        type: 'proxy'
+      },
+      'sony_sab': {
+        url: 'https://bldcmprod-cdn.toffeelive.com/cdn/live/slang/sony_sab_576/sony_sab_576.m3u8?bitrate=500000&channel=sony_sab_576&gp_id=',
+        type: 'proxy'
+      }
+      // 👇 এখানে নতুন চ্যানেল যোগ করুন (শেষ আইটেমের পরে কমা দেবেন না)
+    };
+
+    const match = path.match(/^\/(.+)\.m3u8$/);
+
+    if (match) {
+      const channelName = match[1];
+      const config = CHANNELS[channelName];
+
+      if (!config) {
+        const available = Object.keys(CHANNELS).join(', ');
+        return new Response(`Channel "${channelName}" not found. Available: ${available}`, { status: 404 });
+      }
+
+      // ---------- রিডাইরেক্ট ----------
+      if (config.type === 'redirect') {
+        return Response.redirect(config.url, 307);
+      }
+
+      // ---------- প্রক্সি ----------
+      if (config.type === 'proxy') {
+        const proxyUrl = `https://s2.itcnbd.live/server-2/proxy/${channelName}/playlist?u=${encodeURIComponent(config.url)}`;
+
+        try {
+          const response = await fetch(proxyUrl, {
+            headers: {
+              'User-Agent': 'VLC/3.0.18',
+              'Referer': 'https://www.toffeelive.com/'
+            }
+          });
+
+          if (!response.ok) {
+            return new Response(`Proxy fetch failed: ${response.status}`, { status: response.status });
+          }
+
+          let content = await response.text();
+
+          // সেগমেন্ট রিরাইট (পূর্ণাঙ্গ লিংক বানান)
+          const base = new URL(config.url);
+          const basePath = base.pathname.substring(0, base.pathname.lastIndexOf('/') + 1);
+          const lines = content.split('\n');
+          const newLines = lines.map(line => {
+            const trimmed = line.trim();
+            if (trimmed === '' || trimmed.startsWith('#') || trimmed.match(/^https?:\/\//)) return line;
+            if (trimmed.startsWith('/')) return base.origin + trimmed;
+            return base.origin + basePath + trimmed;
+          });
+          content = newLines.join('\n');
+
+          return new Response(content, {
+            status: 200,
+            headers: {
+              'Content-Type': 'application/vnd.apple.mpegurl',
+              'Cache-Control': 'public, max-age=2, stale-while-revalidate=30',
+              'Access-Control-Allow-Origin': '*'
+            }
+          });
+
+        } catch (error) {
+          return new Response('Proxy Error: ' + error.message, { status: 500 });
+        }
+      }
+    }
+
+    // হোম পেজ – সব ব্রাউজারে খোলা থাকবে
+    const list = Object.keys(CHANNELS).map(ch => `/${ch}.m3u8 (${CHANNELS[ch].type})`).join('\n');
+    return new Response(`✅ Secure Proxy Active.\n\nAvailable Channels:\n${list}\n\n📌 Works in browsers, VLC, MX Player, OttNavigator, and all IPTV players. HTTP Canary blocked.`, { status: 200 });
+  }
+};
