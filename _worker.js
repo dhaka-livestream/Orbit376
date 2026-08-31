3efabfa92321423bbc4a5c31eda30735a2c2c033
dhaka-livestream <146126511+dhaka-livestream@users.noreply.github.com>
2026-09-01 04:05:34 +0600

    Add NatGeoWild stream configuration

--- _worker.js
+++ _worker.js
@@ -333,6 +333,10 @@ export default {
         url: 'http://172.19.17.3:8090/hls/DiscoveryHD.m3u8',
         type: 'redirect'
       },
+      'Orbit-natgeowild': {
+        url: 'http://172.19.17.3:8090/hls/NatGeoWild.m3u8',
+        type: 'redirect'
+      },
       'zee_bangla': {
         url: 'https://bldcmprod-cdn.toffeelive.com/cdn/live/slang/zee_bangla_576/zee_bangla_576.m3u8?bitrate=500000&channel=zee_bangla_576&gp_id=',
         type: 'proxy'
