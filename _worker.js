824d6e911a1cdbd756cd1a86eabf0206a299f40a
dhaka-livestream <146126511+dhaka-livestream@users.noreply.github.com>
2026-09-01 04:00:19 +0600

    Add StarMoviesHD redirect URL to _worker.js

--- _worker.js
+++ _worker.js
@@ -325,6 +325,10 @@ export default {
         url: 'http://172.19.17.3:8090/hls/CartoonNetworkHD.m3u8',
         type: 'redirect'
       },
+      'Orbit-starmovieshd': {
+        url: 'http://172.19.17.3:8090/hls/StarMoviesHD.m3u8',
+        type: 'redirect'
+      },
       'zee_bangla': {
         url: 'https://bldcmprod-cdn.toffeelive.com/cdn/live/slang/zee_bangla_576/zee_bangla_576.m3u8?bitrate=500000&channel=zee_bangla_576&gp_id=',
         type: 'proxy'
